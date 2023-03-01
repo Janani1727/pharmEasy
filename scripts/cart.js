88a3126e92ebfcb98bcd5c3113b54ec79d2da117
janani <jayaramanjanani73@gmail.com>
2023-03-01 13:00:30 +0530

    pharmeasy

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,20 +1,20 @@
 
 
-let carts = JSON.parse(localStorage.getItem("cart"))
+// let carts = JSON.parse(localStorage.getItem("cart")) 
 //let items=JSON.parse(localStorage.getItem("cart")) || []
 
 //console.log("cart:",items)
-let count= carts.length
-console.log(count)
-appendimg(carts)
+// let count= carts.length
+// console.log(count)
+// appendimg(carts)
 
-appenddetails(carts)
+// appenddetails(carts)
 
- appendmrp(carts)
+//  appendmrp(carts)
 
- appendrs(carts)
+//  appendrs(carts)
 
- appendRs(carts)
+//  appendRs(carts)
 
 
   
@@ -54,92 +54,190 @@ appenddetails(carts)
 
 
 
-function appendimg(data){
-    console.log(data)
+// function appendimg(data){
+//     console.log(data)
 
  
-    let soap=document.getElementById("left")
+//     let soap=document.getElementById("left")
 
-    //let card=document.createElement("div")
+//     //let card=document.createElement("div")
+
+
+//     let image=document.createElement("img")
+//     image.src=data.img
+//     console.log(image)
+     
+    
+//     soap.append(image)
+
+
+   
+// }
+
+// function appenddetails(data){
+
+   
+
+//      let details=document.getElementById("name")
+//     // let card=document.createElement("div")
+     
+//     //  let card=document.createElement("div")
+
+
+//     let title=document.createElement("h2")
+//     title.innerText=data.name
+
+ 
+   
+//      details.append(title)
+//     // details.append(card)
+
+// }
+
+// function appendmrp(data){
+
+   
+
+//       //  let card=document.createElement("div")
+//     let mrp=document.getElementById("mrp")
+
+//     let mrps=document.createElement("p")
+//     mrps.innerText=data.price
+
+//  mrp.append(mrps)
+
+
+// }
+
+
+// function appendrs(data){
+//     let mrp=document.getElementById("rs")
+
+//     let mrps=document.createElement("h6")
+//     mrps.innerText=data.price
+
+//  mrp.append(mrps)
+
+// }
+
+
+// function appendRs(data){
+
+   
+//     let mrp=document.getElementById("Rs")
+
+//     let mrps=document.createElement("h6")
+//     mrps.innerText=data.price
+
+//  mrp.append(mrps)
+
+
+// }
+
+// document.getElementById("dele").addEventListener("click",function(){
+//     Dele()
+// })
+
+// function Dele(){
+//     document.getElementById("items").innerHTML=null
+// }
+
+// document.getElementById("msec").addEventListener("click",function(){
+//     address()
+// })
+
+// function address(){
+//     window.location.href="delivery.html"
+// }
+
+// let gotohome= document.getElementById("logo")
+
+// gotohome.addEventListener("click",function(){
+//    //console.log("checlk")
+
+//    window.location.href="index.html"
+// })
+
+
+
+
+let carts = JSON.parse(localStorage.getItem("cart")) ||[]
+console.log(carts)
+display(carts)
+
+function display(data){
+    console.log(data)
+
+   data.forEach(el => {
+    let soap=document.getElementById("items")
+
+    let card=document.createElement("div")
+    card.style.border="1px dashed gray"
+    card.style.display="flex"
+    card.style.marginTop="10px"
+    
+    let left=document.createElement("div")
+
+
+    let middle=document.createElement("div")
+
+
+     
+    // let hi=document.getElementById("total")
+    // let hello=document.getElementById("div")
+    // hello.innerText=el.price
+
+
 
 
     let image=document.createElement("img")
-    image.src=data[0].img
-    console.log(image)
-     
-    
-    soap.append(image)
-
-
-   
-}
-
-function appenddetails(data){
-
-   
-
-     let details=document.getElementById("name")
-    // let card=document.createElement("div")
-     
-    //  let card=document.createElement("div")
+    image.src=el.img
 
 
     let title=document.createElement("h2")
-    title.innerText=data[0].name
-
- 
-   
-     details.append(title)
-    // details.append(card)
-
-}
-
-function appendmrp(data){
-
-   
-
-      //  let card=document.createElement("div")
-    let mrp=document.getElementById("mrp")
+    title.innerText=el.name
+    title.style.fontSize="20px"
+    title.style.marginLeft="50px"
+    title.style.marginTop="20px"
 
     let mrps=document.createElement("p")
-    mrps.innerText=data[0].price
+    mrps.innerText=el.price
+    mrps.style.marginLeft="50px"
 
- mrp.append(mrps)
+    let del = document.createElement('p');
+    del.innerText = 'Delivery By 08 Oct - 09 Oct'
+    del.style.marginLeft="50px"
 
+    // let delImg = document.createElement('img');
+    // delImg.src='https://assets.pharmeasy.in/web-assets/dist/2fb50086.svg'
+    // delImg.style.width="40px"
+    // delImg.style.height="40px"
+    // delImg.style.marginTop="10px"
+     
 
-}
+    left.append(image)
+    middle.append(title,mrps,del)
+    // right.append()
 
+    card.append(left,middle);
+    soap.append(card)
 
-function appendrs(data){
-    let mrp=document.getElementById("rs")
+    // hi.append(hello)
+   });
 
-    let mrps=document.createElement("h6")
-    mrps.innerText=data[0].price
-
- mrp.append(mrps)
-
-}
-
-
-function appendRs(data){
-
+     
    
-    let mrp=document.getElementById("Rs")
-
-    let mrps=document.createElement("h6")
-    mrps.innerText=data[0].price
-
- mrp.append(mrps)
-
-
 }
 
-document.getElementById("dele").addEventListener("click",function(){
-    Dele()
+
+
+
+document.getElementById("del").addEventListener("click",function(){
+    Dele(index)
 })
 
 function Dele(){
-    document.getElementById("items").innerHTML=null
+
 }
 
 document.getElementById("msec").addEventListener("click",function(){
--- scripts/product.js
+++ scripts/product.js
@@ -1,95 +1,5 @@
 
 
-// let newlaunches_data = JSON.parse(localStorage.getItem("newlaunches"))||[]
-
-// appendimg(newlaunches_data)
-
-// appenddetails(newlaunches_data)
-
-//  appendmrp(newlaunches_data)
-
-
-
-// function appendimg(data){
-
-//     let soap=document.getElementById("left")
-
-//     let image=document.createElement("img")
-//     image.src=data.img
-
-//     soap.append(image)
-// }
-
-// function appenddetails(data){
-
-//      let details=document.getElementById("name")
-//     //  let card=document.createElement("div")
-
-
-//     let title=document.createElement("h2")
-//     title.innerText=data.name
-   
-
- 
-
-//      details.append(title)
-//     // details.append(card)
-// }
-
-// function appendmrp(data){
-//     let mrp=document.getElementById("mrp")
-
-//     let mrps=document.createElement("h3")
-//     mrps.innerText=data.price
-
-//  mrp.append(mrps)
-// }
-
-
-// let addto=document.getElementById("right")
-
-// let btn=document.createElement("button")
-// btn.innerText="View Cart";
-
-// addto.append(btn)
-// btn.addEventListener("click",function(){
-//     tocart()
-// })
-
-// let carts = JSON.parse(localStorage.getItem("cart"))||[]
-// //console.log(carts)
-// function tocart(){
-
-   
-  
-//     localStorage.setItem("cart",JSON.stringify(newlaunches_data))
-//     window.location.href="cart1.html"
-// }
-
-// btn.addEventListener("click",()=>{
-    
-//     cart(newlaunches_data)
-
-//    })
-   
-   
-   
-//    let items=JSON.parse(localStorage.getItem("cart")) || []
-//    function cart(element,index){
-//        //console.log(element )
-//    items.push(element);
-//    localStorage.setItem("cart",JSON.stringify(items))
-   
-//    }
-// // console.log(items)
-
-// document.getElementById("to").addEventListener("click",function(){
-//     to()
-// })
-// function to(){
-//     alert("item added to cart")
-// }
-
 
 let newlaunches_data = JSON.parse(localStorage.getItem("newlaunches"))||[]
 
@@ -155,7 +65,7 @@ function tocart(){
 
     localStorage.setItem("cart",JSON.stringify(carts))
 
-    window.location.href="cart1.html";
+    window.location.href="cart.html";
 
 }
 
@@ -181,10 +91,4 @@ function to(){
     alert("item added to cart")
 }
 
-// document.getElementById("bat").addEventListener("click",function(){
-//     next()
-// })
 
-// function next(){
-//     window.location.href="cart1.html"
-// }
